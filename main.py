a55f7834bf9e3b2ab64769783171ec3882854818
Jeremy Njuguna <jgn@udel.edu>
2025-12-06 12:14:53 -0500

    Clean up imports in main.py
    Removed unused imports from main.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ from bakery import assert_equal
 from drafter import *
 from dataclasses import dataclass
 
-from meta import *
-from state import State
-
-
 @route
 def index(state: State) -> Page:
     return Page(state, ["Hello World!"])
